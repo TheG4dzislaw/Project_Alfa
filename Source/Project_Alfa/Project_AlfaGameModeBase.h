03e96773e1fc93cbb0a2b510215eee34f47a9bc5
TheG4dzislaw <g4dzilla@protonmail.com>
2023-06-04 17:17:21 +0200

    Created blank project in Unreal Engine 5

---
+++ Source/Project_Alfa/Project_AlfaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_AlfaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ALFA_API AProject_AlfaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
